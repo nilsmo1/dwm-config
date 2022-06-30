c392d674f1b12c0a33801d1662e219089f0119a2
Nils <nilsmo1@hotmail.com>
2022-07-01 01:25:38 +0200

    Added network connection to bar-xsetroot

--- sh/bar-xsetroot.sh
+++ sh/bar-xsetroot.sh
@@ -2,5 +2,7 @@
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
 charging=$(cat /sys/class/power_supply/BAT0/status)
 date_time=$(date -u +'%A | %d/%m (%B %d) | %R ')
+connection=$(netctl list | awk '/^\*/{for (i=2; i<=NF; i++) printf $i " ";}')
 [[ $charging = "Charging" ]] && out="$bat% (charging) | $date_time" || out="$bat% | $date_time"
-xsetroot -name "$out" 
+[[ $connection ]] && connection="Connection: $connection|"
+xsetroot -name "$connection $out" 
