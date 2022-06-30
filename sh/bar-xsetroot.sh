8bb3f9b6b68db7c229c32f7633cb264bdd88d011
Nils <nilsmo1@hotmail.com>
2022-06-30 19:56:37 +0200

    tweak

--- sh/bar-xsetroot.sh
+++ sh/bar-xsetroot.sh
@@ -2,5 +2,5 @@
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
 charging=$(cat /sys/class/power_supply/BAT0/status)
 date_time=$(date -u +'%A | %d/%m (%B %d) | %R ')
-[[ "$charging" == "Charging" ]] && out="$bat% (charging) | $date_time" || out="$bat% | $date_time"
+[[ $charging = "Charging" ]] && out="$bat% (charging) | $date_time" || out="$bat% | $date_time"
 xsetroot -name "$out" 
