fe6956dbbe53f55d68a3738d813448e246eff117
Nils <nilsmo1@hotmail.com>
2022-06-30 19:47:01 +0200

    bar-xsetroot.sh

--- sh/bar-xsetroot.sh
+++ sh/bar-xsetroot.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
+charging=$(cat /sys/class/power_supply/BAT0/status)
 date_time=$(date -u +'%A | %d/%m (%B %d) | %R ')
-xsetroot -name "$bat% | $date_time"
+[[ "$charging" == "Charging" ]] && out="$bat% (charging) | $date_time" || out="$bat% | $date_time"
+xsetroot -name "$out" 
