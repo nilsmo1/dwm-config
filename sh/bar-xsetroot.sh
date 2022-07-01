0be03cae9b34ce8bc6e2162ce37b8017a2805380
Nils <nilsmo1@hotmail.com>
2022-07-01 02:15:00 +0200

    tweak dates

--- sh/bar-xsetroot.sh
+++ sh/bar-xsetroot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
 charging=$(cat /sys/class/power_supply/BAT0/status)
-date_time=$(date -u +'%A | %d/%m (%B %d) | %R ')
+date_time=$(date -u +'%A | %-d/%-m (%B %-d) | %R ')
 connection=$(netctl list | awk '/^\*/{for (i=2; i<=NF; i++) printf $i " ";}')
-[[ $charging = "Charging" ]] && out="$bat%+ | $date_time" || out="$bat%  | $date_time"
+[[ $charging = "Charging" ]] && out="$bat%+ | $date_time" || out="$bat% | $date_time"
 [[ $connection ]] && connection="Network: $connection|"
 xsetroot -name "$connection $out" 
