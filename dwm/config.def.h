0ed1abf7fc2db0f5ba3569cb539a4292cd5eed71
Nils <nilsmo1@hotmail.com>
2022-07-03 01:40:58 +0200

    added icons with font-awesome

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "InputMono:size=14" };
-static const char dmenufont[]       = "InputMono:size=14";
+static const char *fonts[]          = { "Font Awesome:size=20" };
+static const char dmenufont[]       = "Font Awesome:size=20";
 static const char col_black[]       = "#000000";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
--- sh/bar-xsetroot.sh
+++ sh/bar-xsetroot.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
+bat_icon=$(echo -e "\uf244 \uf243 \uf242 \uf241 \uf240" | awk -v i=$(expr 1 + $bat / 20) '{print $i}')
 charging=$(cat /sys/class/power_supply/BAT0/status)
+c_bat=$(echo -e "\uf0e7") 
 date_time=$(date -u +'%A ┃ %-d/%-m (%B %-d) ┃ %R ')
 connection=$(netctl list | awk '/^\*/{for (i=2; i<=NF; i++) printf $i " ";}')
-[[ $charging = "Charging" ]] && out="$bat%+ ┃ $date_time" || out="$bat% ┃ $date_time"
-[[ $connection ]] && connection="$connection┃"
+[[ $charging = "Charging" ]] && out="$c_bat $bat% ┃ $date_time" || out="$bat_icon $bat% ┃ $date_time"
+[[ $connection ]] && connection="$(echo -e "\uf1eb") $connection┃"
 xsetroot -name "$connection $out" 
