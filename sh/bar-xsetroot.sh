249a96587410495bb62ff1b0e878299c18939ca3
Nils <nilsmo1@hotmail.com>
2022-07-02 22:16:47 +0200

    tweak

--- sh/bar-xsetroot.sh
+++ sh/bar-xsetroot.sh
@@ -4,5 +4,5 @@ charging=$(cat /sys/class/power_supply/BAT0/status)
 date_time=$(date -u +'%A ┃ %-d/%-m (%B %-d) ┃ %R ')
 connection=$(netctl list | awk '/^\*/{for (i=2; i<=NF; i++) printf $i " ";}')
 [[ $charging = "Charging" ]] && out="$bat%+ ┃ $date_time" || out="$bat% ┃ $date_time"
-[[ $connection ]] && connection="Network: $connection┃"
+[[ $connection ]] && connection="$connection┃"
 xsetroot -name "$connection $out" 
