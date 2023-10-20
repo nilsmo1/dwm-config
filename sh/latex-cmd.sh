896c9bda4cfa70327c7fa6b2bcb843121c52adc0
Nils Mo <nilsmo1@hotmail.com>
2023-10-20 17:12:56 +0200

    latex-cmd

---
+++ sh/latex-cmd.sh
@@ -0,0 +1,24 @@
+#/usr/bin/env bash
+
+base='_latex_cmd_'
+tex_output="$base.tex"
+pdf_output="$base.pdf"
+png_output="$base.png"
+
+echo "\documentclass[border=2pt]{standalone}
+\usepackage{amsmath}
+\usepackage{amsfonts}
+\usepackage{amssymb}
+\usepackage{euler}
+\usepackage{newpxtext}
+\usepackage{mathpartir}
+\usepackage{stmaryrd}
+\usepackage{varwidth}
+\begin{document}
+\begin{varwidth}{\linewidth}
+$@
+\end{varwidth}
+\end{document}" > $tex_output && pdflatex --halt-on-error $tex_output > /dev/null &&
+convert -density 300 $pdf_output -quality 500 $png_output &&
+xclip -selection clipboard -t image/png -i $png_output &&
+rm -f $base.* || echo "something went wrong!"
