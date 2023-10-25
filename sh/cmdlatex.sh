0b2b91aa7ca35598ca8d9e9533256753606f71b3
nils mo <nilsmo1@hotmail.com>
2023-10-25 15:06:42 +0200

    cmdlatex with -i nvim box

--- sh/cmdlatex.sh
+++ sh/cmdlatex.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 base='_latex_cmd_tmp_file_'
+input_file=$base'input_.tex'
 tex_output="$base.tex"
 pdf_output="$base.pdf"
 png_output="$base.png"
+
+[ "$#" -lt 1 ] && exit 1
+
+case $1 in
+    '-i') nvim $input_file && input=$(cat $input_file) || exit 1 ;;
+      * ) input=$@ ;;
+esac
+rm -f $input_file
 
 echo "\documentclass[border=2pt]{standalone}
 \usepackage{amsmath}
@@ -16,11 +25,13 @@ echo "\documentclass[border=2pt]{standalone}
 \usepackage{varwidth}
 \begin{document}
 \begin{varwidth}{\linewidth}
-$@
+$input
 \end{varwidth}
 \end{document}" > $tex_output && pdflatex --halt-on-error $tex_output > /dev/null &&
-convert -density 300 $pdf_output -quality 500 $png_output &&
+convert -density 600 $pdf_output -quality 600 $png_output &&
 xclip -selection clipboard -t image/png -i $png_output ||
 echo "something went wrong!"
-feh $png_output
+if test -f $png_output; then
+    feh $png_output
+fi
 rm -f $base.*
